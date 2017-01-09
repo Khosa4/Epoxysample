bbf03a90e43c051bb5d29d98309fa31150af609f
Dar <khanmomna1@gmail.com>
2017-01-09 16:30:40 +0500

    Initial commit

---
+++ app/src/main/java/example/com/epoxysample/models/ButtonModel.java
@@ -0,0 +1,43 @@
+package com.airbnb.epoxy.models;
+
+import android.support.annotation.StringRes;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.airbnb.epoxy.EpoxyAttribute;
+import com.airbnb.epoxy.EpoxyModelWithHolder;
+import com.airbnb.epoxy.R;
+import com.airbnb.epoxy.models.ButtonModel.ButtonHolder;
+
+import butterknife.BindView;
+
+/** This model class gives an example of how to use a view holder pattern with your models. */
+public class ButtonModel extends EpoxyModelWithHolder<ButtonHolder> {
+  @EpoxyAttribute @StringRes int text;
+  @EpoxyAttribute OnClickListener clickListener;
+
+  @Override
+  protected int getDefaultLayout() {
+    return R.layout.model_button;
+  }
+
+  @Override
+  public int getSpanSize(int totalSpanCount, int position, int itemCount) {
+    return totalSpanCount;
+  }
+
+  @Override
+  public void bind(ButtonHolder holder) {
+    holder.button.setText(text);
+    holder.button.setOnClickListener(clickListener);
+  }
+
+  @Override
+  protected ButtonHolder createNewHolder() {
+    return new ButtonHolder();
+  }
+
+  static class ButtonHolder extends BaseEpoxyHolder {
+    @BindView(R.id.button) Button button;
+  }
+}
---
+++ app/src/main/java/example/com/epoxysample/models/HeaderModel.java
@@ -0,0 +1,34 @@
+package com.airbnb.epoxy.models;
+
+import android.support.annotation.StringRes;
+
+import com.airbnb.epoxy.EpoxyAttribute;
+import com.airbnb.epoxy.EpoxyModel;
+import com.airbnb.epoxy.R;
+import com.airbnb.epoxy.views.HeaderView;
+
+/**
+ * This model shows an example of binding to a specific view type. In this case it is a custom view
+ * we made, but it could also be another single view, like an EditText or Button.
+ */
+public class HeaderModel extends EpoxyModel<HeaderView> {
+  @EpoxyAttribute @StringRes int title;
+  @EpoxyAttribute @StringRes int caption;
+
+  @Override
+  protected int getDefaultLayout() {
+    return R.layout.model_header;
+  }
+
+  @Override
+  public void bind(HeaderView view) {
+    view.setTitle(title);
+    view.setCaption(caption);
+  }
+
+  @Override
+  public int getSpanSize(int totalSpanCount, int position, int itemCount) {
+    // We want the header to take up all spans so it fills the screen width
+    return totalSpanCount;
+  }
+}
